73b2a73611df475e6f86f2e11f37f3e7bc64859a
HoMaiQue <homaique.it@gmail.com>
2022-11-26 23:54:03 +0700

    add register layout

--- src/layouts/RegisterLayout/RegisterLayout.tsx
+++ src/layouts/RegisterLayout/RegisterLayout.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react'
+import Footer from 'src/components/Footer'
+import RegisterHeader from 'src/components/RegisterHeader'
 
 export interface RegisterProps {
   children: React.ReactNode
 }
 
 export default function RegisterLayout({ children }: RegisterProps) {
-  return <div>{children}</div>
+  return (
+    <div>
+      <RegisterHeader />
+      {children}
+      <Footer />
+    </div>
+  )
 }
