3b97269162f5e9a25fd6e13fc46c30b4c064453b
HoMaiQue <homaique.it@gmail.com>
2023-01-14 23:05:25 +0700

    add ui profile

--- src/pages/User/components/UserSideNav/UserSideNav.tsx
+++ src/pages/User/components/UserSideNav/UserSideNav.tsx
@@ -1,5 +1,93 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { path } from 'src/constant'
 
 export default function UserSideNav() {
-  return <div>UserSideNav</div>
+  return (
+    <div>
+      <div className='flex items-center border-b border-b-gray-200 py-4'>
+        <Link to={path.profile} className='h-12 w-12 flex-shrink-0 overflow-hidden rounded-full border border-black/10'>
+          <img
+            src='https://cf.shopee.vn/file/d04ea22afab6e6d250a370d7ccc2e675_tn'
+            alt='avatar'
+            className='h-full w-full object-cover'
+          />
+        </Link>
+        <div className='flex-grow pl-4'>
+          <div className='mb-1 truncate font-semibold text-gray-600'>homaique2000</div>
+          <Link to={path.profile} className='flex items-center capitalize text-gray-500 '>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='h-4 w-4'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125'
+              />
+            </svg>
+            Sửa hồ sơ
+          </Link>
+        </div>
+      </div>
+      <div className='mt-7'>
+        <Link to={path.changePassword} className='flex items-center gap-x-2 capitalize text-orange transition-colors'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='h-5 w-5 stroke-blue-400'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z'
+            />
+          </svg>
+          <span className='flex-grow'>Đổi mật khẩu</span>
+        </Link>
+        <Link to={path.profile} className='mt-4 flex items-center gap-x-2 capitalize transition-colors'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='h-5 w-5 stroke-blue-400'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184'
+            />
+          </svg>
+
+          <span className='flex-grow'>Đơn Mua</span>
+        </Link>
+        <Link to={path.profile} className='mt-4 flex items-center gap-x-2 capitalize  transition-colors'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='h-5 w-5 stroke-blue-400'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z'
+            />
+          </svg>
+          <span className='flex-grow'>Tài khoản của tôi</span>
+        </Link>
+      </div>
+    </div>
+  )
 }
--- src/pages/User/pages/Profile/Profile.tsx
+++ src/pages/User/pages/Profile/Profile.tsx
@@ -1,6 +1,81 @@
-import * as React from 'react'
-
+import Input from 'src/components/Input'
 
 export function Profile() {
-  return <div>profile</div>
+  return (
+    <div className='rounded-sm bg-white px-2 pb-10 shadow md:px-7 md:pb-20'>
+      <div className='border-b border-b-gray-200 py-6'>
+        <h1 className='text-lg font-medium capitalize text-gray-900'>hồ sơ của tôi</h1>
+        <div className='mt-1 text-sm text-gray-700'>Quản lý thông tin hồ sơ để bảo mật tài khoản</div>
+        <div className='mt-8 flex flex-col-reverse md:flex-row  md:items-start'>
+          <form className='mt-6 flex-grow md:mt-0  md:pr-12'>
+            <div className='flex flex-col flex-wrap  sm:flex-row'>
+              <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Email</div>
+              <div className='sm:w-[80%] sm:pl-5'>
+                <div className='pt-3 text-gray-700'>homaique@gmail.com</div>
+              </div>
+            </div>
+            <div className='mt-2 flex flex-col flex-wrap  sm:flex-row'>
+              <div className='truncate pt-3 capitalize sm:w-[20%]  sm:text-right'>Tên</div>
+              <div className='sm:w-[80%] sm:pl-5'>
+                <Input classNameInput='w-full rounded-sm border border-gray-300 p-2 outline-none focus:border-gray-500 focus:shadow-sm'></Input>
+              </div>
+            </div>
+            <div className='mt-2 flex flex-col flex-wrap  sm:flex-row'>
+              <div className='truncate pt-3 capitalize sm:w-[20%]  sm:text-right'>Số điện thoại</div>
+              <div className='sm:w-[80%] sm:pl-5'>
+                <Input classNameInput='w-full rounded-sm border border-gray-300 p-2 outline-none focus:border-gray-500 focus:shadow-sm'></Input>
+              </div>
+            </div>
+            <div className='mt-2 flex flex-col flex-wrap  sm:flex-row'>
+              <div className='truncate pt-3 capitalize sm:w-[20%]  sm:text-right'>Địa chỉ</div>
+              <div className='sm:w-[80%] sm:pl-5'>
+                <Input classNameInput='w-full rounded-sm border border-gray-300 p-2 outline-none focus:border-gray-500 focus:shadow-sm'></Input>
+              </div>
+            </div>
+            <div className='mt-2 flex flex-col flex-wrap  sm:flex-row'>
+              <div className='truncate pt-3 capitalize sm:w-[20%]  sm:text-right'>Ngày sinh</div>
+              <div className='sm:w-[80%] sm:pl-5'>
+                <div className='flex justify-between'>
+                  <select name='' id='' className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                    <option value='' disabled>
+                      Ngày
+                    </option>
+                  </select>
+                  <select name='' id='' className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                    <option value='' disabled>
+                      Tháng
+                    </option>
+                  </select>
+                  <select name='' id='' className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                    <option value='' disabled>
+                      Năm
+                    </option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </form>
+          <div className='flex justify-center md:w-72 md:border-l md:border-l-gray-200'>
+            <div className='flex flex-col items-center '>
+              <div className='my-5 h-24 w-24'>
+                <img
+                  src='https://cf.shopee.vn/file/d04ea22afab6e6d250a370d7ccc2e675_tn'
+                  className='h-full w-full rounded-full object-cover'
+                  alt='avatar'
+                />
+              </div>
+              <input type='file' accept='.jpg,.jpeg,.png' className='hidden' />
+              <button className='flex h-10 items-center justify-end rounded-sm border bg-white px-6 text-sm text-gray-600 shadow-sm'>
+                Chọn ảnh
+              </button>
+              <div className='mt-3 text-gray-400'>
+                <div> Dung lượng file tối đa 1MB</div>
+                <div>Định dạng:.JPEG, .PNG</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
